565f86794577c12c50c0eff425799087b9be3d75
Parag Sharma <sharmaparag2004@gmail.com>
2025-03-08 14:06:42 +0530

    new

--- ArrayMethod.js
+++ ArrayMethod.js
@@ -7,7 +7,27 @@
 let nums = [1,2,3,4,5,6,7,8,9,10];
 
 let newarr = nums.map((val) => {
-    return val;
+    return val*val;
 });
 
 console.log(newarr);
+console.log(nums);
+
+// filter -> creates a new array pf elements that give true for a condition/filter, ex. all even elements.
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+let even = arr.filter((val) => {
+    return val%2 ===0;
+})
+console.log(even);
+
+//reduce method --> performers some operation on the array and returns a single value. It takes a callback function as an argument and runs that callback function on each element present in the array. It returns a single value which is the result of the operation performed by the callback function.
+
+let arrr = [1,2,3,4];
+
+const output = arrr.reduce((res, curr) => {
+    return res + curr;
+});
+
+console.log(output); 
